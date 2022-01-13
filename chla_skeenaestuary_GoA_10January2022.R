41b1a1e4ff7f644ff71d885133442e36fc7345e8
CelesteKieran <61250454+CelesteKieran@users.noreply.github.com>
2022-01-13 14:46:16 -0800

    Add files via upload

---
+++ chla_skeenaestuary_GoA_10January2022.R
@@ -0,0 +1,15 @@
+## Chlorophyll a
+
+#### NASA workflow ####
+# Go to https://earthdata.nasa.gov/earth-observation-data/find-data/idn (International Directory Network [IDN])
+
+
+#### What coordinates do I want to check first? 
+
+## Skeena Estuary
+#Estuary ETM (rough estimates PSF estuary technical report)
+#Zone 9N
+#min 372000 W, 5938500 N
+#max 412000 W, 6058500N
+
+
